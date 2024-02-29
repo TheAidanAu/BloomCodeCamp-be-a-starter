b7c5a3c6fc1986db8e4e8d68e39339bc7c1326e5
Aidan Au <aidan.au.linkedin@gmail.com>
2024-02-29 09:24:38 -0500

    Ticket 2 Subtasks finished

--- src/main/java/com/hcc/repositories/UserRepository.java
+++ src/main/java/com/hcc/repositories/UserRepository.java
@@ -1,8 +1,8 @@
 package com.hcc.repositories;
 
 import com.hcc.entities.User;
-import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
@@ -10,5 +10,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     // JpaRepository provides the most common CRUD operations
     Optional<User> findByUsername(String username);
-
 }
+
+
